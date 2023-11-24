253f4cd0c6da87623330f0f50e721de703245ee2
nidhiBhatia <nidhibhatia7078@gmail.com>
2023-11-23 22:03:40 -0800

    footer donw

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,8 +1,23 @@
 import React from "react";
+import { Typography } from "@mui/material";
 import "./footer.css";
 
-const Footer = ({ children }) => {
-  return <h2 className="footer">{children}</h2>;
+
+const Footer = (props) => {
+  let { logo, summary } = props;
+  console.log('props', props)
+  return (
+    <footer>
+      <div className="footer_div">
+        <Typography variant="h5" className="logoText">
+          {logo}
+        </Typography>
+        <Typography variant="body1" className="summary_text" >
+          {summary}
+        </Typography>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/components/Footer/footer.stories.js
+++ src/components/Footer/footer.stories.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import Footer from "./footer";
 
 export default {
@@ -7,4 +6,16 @@ export default {
   component: Footer,
 };
 
-export const Text = () => <Footer> Footer</Footer>;
+const template = (args) => <Footer {...args} />;
+
+export const FooterComponent = template.bind({});
+
+FooterComponent.args={
+  logo:"Company Name",
+  summary:`It is a long established fact that a reader will be distracted by the
+  readable content of a page when looking at its layout. The point of
+  using Lorem Ipsum is that it has a more-or-less normal distribution of
+  letters, as opposed to using 'Content here, content here', making it
+  look like readable English.`
+}
+
